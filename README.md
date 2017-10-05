0f96c58d77211a9511ef2177c82750d17f5fbf72
Tommy Hodgins <tomhodgins@gmail.com>
2017-10-04 23:34:47 -0400

    Add initial files ok 😘👌

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # todoml
-TodoML - a simple markdown-like syntax for composing todo lists
+
+**a simple markdown-like syntax for composing todo lists**
+
+## Usage
+
+Write todoML inside <script type="text/todo"> tags inside HTML and include `todoml.js`:
+
+```html
+<script type="text/todo">
+
+  This is a TodoML document
+
+</script>
+
+<script src=todoml.js></script>
+```
+
+## Syntax
+
+TodoML is similar to markdown, but much more simplified. It's a Domain-Specific Language created for writing todo lists and exists as a superset of HTML, so you're able to use any HTML you like in addition to these shorthands.
+
+Within a TodoML document, each line is considered a separate thing. What HTML a line becomes is determined by the first few characters from the start of the line.
+
+### Paragraph
+
+Lines with no special characters in TodoML get wrapped in HTML `<p>` tags.
+
+### Headings
+
+Lines beginning with 1-6 `#` charcters followed by a space (`# `, `## `, `### `, `#### `, `##### `, `###### `) in TodoML get wrapper with the corresponding HTML heading tag from `<h1>`-`<h6>`.
+
+### Lists
+
+Lines starting with `- ` in TodoML get wrapped in HTML `<ul><li>` tags.
+
+### Checkboxes
+
+Lines starting with `- [ ]` or `- [x]` become checkboxes, and get wrapped in `<label><ul><li>` with an `<input type=checkbox>` or `<input type=checkbox checked>` depending on whether the `[x]` is present or not.
+
+### Blockquote
+
+Lines beginning with `> ` get wrapped in HTML `<blockquote>` tags.
+
+## Demo
+
+- [TodoML demo page](https://tomhodgins.github.io/todoml/)
---
+++ todoml.js
@@ -0,0 +1,85 @@
+/*
+
+# TodoML Interpreter
+version 0.0.1
+
+A lightweight markdown-like markup language for todo lists
+
+## Usage
+
+Write todoML inside <script type="text/todo"> tags inside HTML and include this JavaScript.
+
+- author: Tommy Hodgins
+- license: MIT
+
+*/
+
+function todoML() {
+
+  var script = document.querySelectorAll('script[type="text/todo"]')
+
+  for (var i=0; i<script.length; i++) {
+
+    var output = document.createElement('div')
+    var source = script[i].textContent.split('\n')
+    var todoDoc = ''
+
+    for (var j=0; j<source.length; j++) {
+
+      var line = source[j].replace(/^\s+/, '')
+
+      // H1
+      ;/^# /.test(line)
+      && (todoDoc += '<h1>' + line.replace(/^# /, '') + '</h1>')
+
+      // H2
+      ;/^## /.test(line)
+      && (todoDoc += '<h2>' + line.replace(/^## /, '') + '</h2>')
+
+      // H3
+      ;/^### /.test(line)
+      && (todoDoc += '<h3>' + line.replace(/^### /, '') + '</h3>')
+
+      // H4
+      ;/^#### /.test(line)
+      && (todoDoc += '<h4>' + line.replace(/^#### /, '') + '</h4>')
+
+      // H5
+      ;/^##### /.test(line)
+      && (todoDoc += '<h5>' + line.replace(/^##### /, '') + '</h5>')
+
+      // H6
+      ;/^###### /.test(line)
+      && (todoDoc += '<h6>' + line.replace(/^###### /, '') + '</h6>')
+
+      // Paragraph
+      ;/^[^#>`-]/.test(line)
+      && (todoDoc += '<p>' + line + '</p>')
+
+      // Blockquote
+      ;/^\> /.test(line)
+      && (todoDoc += '<blockquote>' + line.replace(/^\> /, '') + '</blockquote>')
+
+      // List item
+      ;/^- [^\[]/.test(line)
+      && (todoDoc += '<ul><li>' + line.replace(/^- /, '') + '</li></ul>')
+
+      // Empty checkbox
+      ;/^- \[ \]/.test(line)
+      && (todoDoc += '<label><ul><li><input type=checkbox>' + line.replace(/^- \[ \]/, '') + '</li></ul></label>')
+
+      // Checked checkbox
+      ;/^- \[x\]/.test(line)
+      && (todoDoc += '<label><ul><li><input type=checkbox checked>' + line.replace(/^- \[x\]/, '') + '</li></ul></label>')
+
+    }
+
+    output.innerHTML = todoDoc
+
+    script[i].parentNode.insertBefore(output, script[i])
+
+  }
+
+}
+
+window.addEventListener('load', todoML)
